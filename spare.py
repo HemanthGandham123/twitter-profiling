f73c84167dfb4f471061be5f433735d8b19ff4dd
Hemanth Gandham <bgandham@ymail.com>
2017-06-25 04:11:51 +0530

    removed bug and updated node properties
    the random ordering of dict keys in mentions has been resolved  and the node 's primary property has been  changed from id to name
    .

--- spare.py
+++ spare.py
@@ -11,26 +11,30 @@ def tofrac(a):
     d=c/b
     return float(d)
 #Relationship(a, "similartags", b, weight = tcount)
-node=Node("Ariana",id=0,name="zero_node",type=0,date=0)   
+node=Node("Ariana",id=0,name="zero_node",type=0,date=0)
 graph.create(node)
 array=[]
 rel_ind=1
 rel_type=0
+score={}
+tmp={}
 n4= graph.find_one("Ariana",property_key = 'id', property_value = 0)
 for fname in iglob(os.path.expanduser('output1/*.json')):
     with open(fname) as fin:
-        print fname
+        print (fname)
         hf=1
         mf=1
         tweets= json.load(fin)
         array.append(tweets)
-        auth_id=tweets['meta']['author_id'],
-        print auth_id
+        auth_id=tweets['meta']['author_id']
+        auth_name=tweets['meta']['author_name']
+        #print (auth_id)
         dt=tweets['meta']['date']['$date']
-        if(graph.find_one("Ariana",property_key = 'id', property_value = auth_id)<0):
-            node = Node("Ariana",id=auth_id, name=tweets['meta']['author_name'],bundle_id=tweets['meta']['retweetOf'],type=1)
+        if(graph.find_one("Ariana",property_key = 'name', property_value = auth_name)==None):
+            node = Node("Ariana",id=auth_id, name=tweets['meta']['author_name'],bundle_id=tweets['meta']['retweetOf'])
             graph.create(node)
-        n1=graph.find_one("Ariana",property_key = 'id', property_value = auth_id)    
+            #score.setdefault(auth_name,{})
+        n1= graph.find_one("Ariana",property_key = 'name', property_value = auth_name)
         #if(dt>n1['date']):
          #   n1['date']=dt
         nh=[]
@@ -38,45 +42,62 @@ for fname in iglob(os.path.expanduser('output1/*.json')):
             for hshtgo in tweets['text']['entities']['hashtags']:
                 hshtg=hshtgo['text']
                 hf=0
-                if(graph.find_one("Ariana",property_key='text',property_value=hshtg)<0):
+                if(graph.find_one("Ariana",property_key='text',property_value=hshtg)==None):
                     node=Node("Ariana",text=hshtg,type=2)
                     graph.create(node)
+                score.setdefault(auth_name,{}).setdefault(hshtg,0)
+                score[auth_name][hshtg]+=8
+                score.setdefault(hshtg,{}).setdefault(auth_name,0)
+                score[hshtg][auth_name]+=8
                 n2=graph.find_one("Ariana",property_key='text',property_value=hshtg)
                 nh.append(n2)
                 r_id=tofrac(rel_ind)+1
                 st=str(r_id)
                 rel_ind=rel_ind+1
-                rel=Relationship(n1,st,n2,content=tweets['text']['content'])   
+                rel=Relationship(n1,st,n2,content=tweets['text']['content'])
                 graph.create(rel)
-                print 'hi'
+                #print ('hi')
                 hf=0
             for i in nh:
                 for j in nh:
                     if(i!=j):
+                        ina=i["text"]
+                        jna=j["text"]
+                        score.setdefault(ina,{}).setdefault(jna,0)
+                        score[ina][jna]+=4
                         r_id=tofrac(rel_ind)+5
                         st=str(r_id)
                         rel_ind=rel_ind+1
                         rel=Relationship(i,st,j,content=tweets['text']['content'])
                         graph.create(rel)
-        nm=[]   
-        if tweets['text']['entities']['mention']:
-            for ment in tweets['text']['entities']['mention']:
-                mnt=ment.values()
-                m_id=str(mnt[2])
-                m_name=str(mnt[4])
-                if(graph.find_one("Ariana",property_key = 'id', property_value =m_id)<0):
-                    node = Node("Ariana", name=m_name,id=m_id,type=3)
+        nm=[]
+        mn_dict=tweets['text']['entities']['mention']
+        if (mn_dict !=None):
+            mndx=0
+            for ment in mn_dict:
+                m_name=mn_dict[mndx]['screen_name']
+                print (m_name)
+                if(graph.find_one("Ariana",property_key = 'name', property_value =m_name)==None):
+                    node = Node("Ariana", name=m_name,type=3)
                     graph.create(node)
-                n3= graph.find_one("Ariana",property_key = 'id', property_value = m_id)
+                n3= graph.find_one("Ariana",property_key = 'name', property_value =m_name )
                 nm.append(n3)
+                score.setdefault(auth_name,{}).setdefault(m_name,0)
+                score[auth_name][m_name]+=8
+                score.setdefault(m_name,{}).setdefault(auth_name,0)
+                score[m_name][auth_name]+=8
                 r_id=tofrac(rel_ind)+2
                 st=str(r_id)
                 rel_ind=rel_ind+1
-                rel=Relationship(n1,st,n3,content=tweets['text']['content'])   
+                rel=Relationship(n1,st,n3,content=tweets['text']['content'])
                 graph.create(rel)
                 for i in nm:
                     for j in nm:
                         if(i!=j):
+                            ina=i["text"]
+                            jna=j["text"]
+                            score.setdefault(ina,{}).setdefault(jna,0)
+                            score[ina][jna]+=4
                             r_id=tofrac(rel_ind)+6
                             st=str(r_id)
                             rel_ind=rel_ind+1
@@ -86,25 +107,31 @@ for fname in iglob(os.path.expanduser('output1/*.json')):
                     #mention and hashtag
                     for i in nm:
                         for j in nh:
+                            ina=i["text"]
+                            jna=j["text"]
+                            score.setdefault(ina,{}).setdefault(jna,0)
+                            score[ina][jna]+=5
                             r_id=tofrac(rel_ind)+3
                             st=str(r_id)
                             rel_ind=rel_ind+1
-                            rel=Relationship(i,st,j,content=tweets['text']['content'])   
+                            rel=Relationship(i,st,j,content=tweets['text']['content'])
                             graph.create(rel)
-                mf=0    
+                mf=0
         if(hf==1 and mf==1):
             r_id=tofrac(rel_ind)
             st=str(r_id)
             rel_ind=rel_ind+1
-            rel=Relationship(n1,st,n4,content=tweets['text']['content'])   
+            rel=Relationship(n1,st,n4,content=tweets['text']['content'])
             graph.create(rel)
 for fname in iglob(os.path.expanduser('output1/*.json')):
     with open(fname) as fin:
         tweets= json.load(fin)
         array.append(tweets)
         auth_id=tweets['meta']['author_id']
-        print auth_id
-        if (graph.find_one("Ariana",property_key = 'id', property_value = auth_id)<0):
+        auth_name=tweets['meta']['author_name']
+        #print (auth_id)
+        n1=graph.find_one("Ariana",property_key = 'name', property_value = auth_name)
+        if (n1==None):
             hf=1
         else :
             rtof=tweets['meta']['retweetOf']
@@ -115,5 +142,13 @@ for fname in iglob(os.path.expanduser('output1/*.json')):
                 rel_ind=rel_ind+1
                 rel=Relationship(n1,st,n2,content=tweets['text']['content'])
                 graph.create(rel)
-
-
+                jna=n2['name']
+                score.setdefault(auth_name,{}).setdefault(jna,0)
+                score[auth_name][jna]+=4
+#print (score)
+qry="ImHagarwal"
+closer=sorted(score[qry].items(), key=lambda x: x[1],reverse=True)
+closest=closer[:2]
+print (score)
+for i in closest:
+        print (i)
