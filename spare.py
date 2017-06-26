4b37ac910881eb10e608e10c48b1a177d3cb32da
Hemanth Gandham <bgandham@ymail.com>
2017-06-27 04:25:54 +0530

    retweet portion in the same scan
    rel_branch manual merge also

--- spare.py
+++ spare.py
@@ -11,17 +11,19 @@ def tofrac(a):
     d=c/b
     return float(d)
 #Relationship(a, "similartags", b, weight = tcount)
-node=Node("Ariana",id=0,name="zero_node",type=0,date=0)
+node=Node("Db_old",id=0,name="zero_node",type=0,date=0)
 graph.create(node)
 array=[]
 rel_ind=1
 rel_type=0
 score={}
 tmp={}
-n4= graph.find_one("Ariana",property_key = 'id', property_value = 0)
+#dic created
+retweet_rel={}
+n4= graph.find_one("Db_old",property_key = 'id', property_value = 0)
 for fname in iglob(os.path.expanduser('output1/*.json')):
     with open(fname) as fin:
-        print (fname)
+        #print (fname)
         hf=1
         mf=1
         tweets= json.load(fin)
@@ -30,26 +32,30 @@ for fname in iglob(os.path.expanduser('output1/*.json')):
         auth_name=tweets['meta']['author_name']
         #print (auth_id)
         dt=tweets['meta']['date']['$date']
-        if(graph.find_one("Ariana",property_key = 'name', property_value = auth_name)==None):
-            node = Node("Ariana",id=auth_id, name=tweets['meta']['author_name'],bundle_id=tweets['meta']['retweetOf'])
+        if(graph.find_one("Db_old",property_key = 'name', property_value = auth_name)==None):
+            node = Node("Db_old",id=auth_id, name=tweets['meta']['author_name'],bundle_id=tweets['meta']['retweetOf'])
             graph.create(node)
             #score.setdefault(auth_name,{})
-        n1= graph.find_one("Ariana",property_key = 'name', property_value = auth_name)
+        n1= graph.find_one("Db_old",property_key = 'name', property_value = auth_name)
         #if(dt>n1['date']):
          #   n1['date']=dt
+        #adding data to dic from each file
+        rtof=tweets['meta']['retweetOf']
+        if not(rtof is None):
+            retweet_rel[auth_id]=rtof
         nh=[]
         if tweets['text']['entities']['hashtags']:
             for hshtgo in tweets['text']['entities']['hashtags']:
                 hshtg=hshtgo['text']
                 hf=0
-                if(graph.find_one("Ariana",property_key='text',property_value=hshtg)==None):
-                    node=Node("Ariana",text=hshtg,type=2)
+                if(graph.find_one("Db_old",property_key='text',property_value=hshtg)==None):
+                    node=Node("Db_old",text=hshtg,type=2)
                     graph.create(node)
                 score.setdefault(auth_name,{}).setdefault(hshtg,0)
                 score[auth_name][hshtg]+=8
                 score.setdefault(hshtg,{}).setdefault(auth_name,0)
                 score[hshtg][auth_name]+=8
-                n2=graph.find_one("Ariana",property_key='text',property_value=hshtg)
+                n2=graph.find_one("Db_old",property_key='text',property_value=hshtg)
                 nh.append(n2)
                 r_id=tofrac(rel_ind)+1
                 st=str(r_id)
@@ -76,11 +82,11 @@ for fname in iglob(os.path.expanduser('output1/*.json')):
             mndx=0
             for ment in mn_dict:
                 m_name=mn_dict[mndx]['screen_name']
-                print (m_name)
-                if(graph.find_one("Ariana",property_key = 'name', property_value =m_name)==None):
-                    node = Node("Ariana", name=m_name,type=3)
+                #print (m_name)
+                if(graph.find_one("Db_old",property_key = 'name', property_value =m_name)==None):
+                    node = Node("Db_old", name=m_name,type=3)
                     graph.create(node)
-                n3= graph.find_one("Ariana",property_key = 'name', property_value =m_name )
+                n3= graph.find_one("Db_old",property_key = 'name', property_value =m_name )
                 nm.append(n3)
                 score.setdefault(auth_name,{}).setdefault(m_name,0)
                 score[auth_name][m_name]+=8
@@ -123,6 +129,8 @@ for fname in iglob(os.path.expanduser('output1/*.json')):
             rel_ind=rel_ind+1
             rel=Relationship(n1,st,n4,content=tweets['text']['content'])
             graph.create(rel)
+
+'''
 for fname in iglob(os.path.expanduser('output1/*.json')):
     with open(fname) as fin:
         tweets= json.load(fin)
@@ -130,13 +138,13 @@ for fname in iglob(os.path.expanduser('output1/*.json')):
         auth_id=tweets['meta']['author_id']
         auth_name=tweets['meta']['author_name']
         #print (auth_id)
-        n1=graph.find_one("Ariana",property_key = 'name', property_value = auth_name)
+        n1=graph.find_one("Db_old",property_key = 'name', property_value = auth_name)
         if (n1==None):
             hf=1
         else :
             rtof=tweets['meta']['retweetOf']
             if not(rtof is None):
-                n2=graph.find_one("Ariana",property_key='bundle_id',property_value=rtof)
+                n2=graph.find_one("Db_old",property_key='bundle_id',property_value=rtof)
                 r_id=tofrac(rel_ind)+4
                 st=str(r_id)
                 rel_ind=rel_ind+1
@@ -145,10 +153,24 @@ for fname in iglob(os.path.expanduser('output1/*.json')):
                 jna=n2['name']
                 score.setdefault(auth_name,{}).setdefault(jna,0)
                 score[auth_name][jna]+=4
+'''
 #print (score)
+#for each element in dic creating relationships
+for my_var in retweet_rel:re
+    n1=graph.find_one("Db_old",property_key = 'id', property_value = my_var)
+    n2=graph.find_one("Db_old",property_key='bundle_id',property_value=retweet_rel[my_var])
+    r_id=tofrac(rel_ind)+4
+    st=str(r_id)
+    rel_ind=rel_ind+1
+    rel=Relationship(n1,st,n2,content=tweets['text']['content'])
+    graph.create(rel)
+    jna=n2['name']
+    score.setdefault(auth_name,{}).setdefault(jna,0)
+    score[auth_name][jna]+=4
+    print my_var, 'corresponds to', retweet_rel[my_var]
 qry="ImHagarwal"
 closer=sorted(score[qry].items(), key=lambda x: x[1],reverse=True)
 closest=closer[:2]
-print (score)
+#print (score)
 for i in closest:
         print (i)
